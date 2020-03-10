25cebb27867e864272c4160a7a1920a5641f2e11
Rodriguez <juan.b.rodriguez@accenture.com>
2020-03-10 13:27:49 -0500

    Cambio de titulo dentro de /components

--- orch_front/src/App.js
+++ orch_front/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Title from './components/Title';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Title></Title>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ orch_front/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Title extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Orquestador</h1>
+                <p>Este es el front del orquestador</p>
+            </div>
+        );
+    }
+}
