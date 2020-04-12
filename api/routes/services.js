67355b213b3a42d6d4fa1ac6d98b2565b035fb3d
xionjames <jhon.freitez@gmail.com>
2020-04-12 11:57:37 -0500

    services/getById

--- api/routes/services.js
+++ api/routes/services.js
@@ -1,16 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-async function getServices() {
-  var mongoConnection = require('../db/connection.js'); 
-  return await mongoConnection.find('ORCH_SERVICES', {}, {id: 1, _id: 0, keys: 1, description: 1}, {id: 1});
-}
-
 /* GET * listing. */
 router.get('/getAll', async function(req, res, next) {
-  var docs = await getServices();
-  console.log("Found records");
-  console.log(docs);
+  var mongoConnection = require('../db/connection.js'); 
+
+  var docs = await mongoConnection.find(
+    'ORCH_SERVICES',
+    {}, 
+    {id: 1, _id: 0, keys: 1, description: 1}, 
+    {id: 1}
+  );
+  console.log("Found " + docs.length + " records");
+
+  res.json({
+    response: "OK",
+    description: "OK",
+    documents: docs
+  });
+
+});
+
+
+/**
+ * api/services/getById?id=CollProcesaRmca
+ */
+router.get('/getById', async function(req, res, next) {
+  var serviceId = req.query.id;
+
+  var mongoConnection = require('../db/connection.js'); 
+
+  var docs = await mongoConnection.find(
+    'ORCH_SERVICES',
+    {id: serviceId}
+  );
+  console.log("Found record! ");
 
   res.json({
     response: "OK",
